3154bcb61f7f592b897382bbe1dc2724a4a7f787
João Victor Matulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2023-05-12 11:31:39 -0300

    Update CriarAcesso.java

--- ProdEq/src/View/Dono/CriarAcesso.java
+++ ProdEq/src/View/Dono/CriarAcesso.java
@@ -12,7 +12,7 @@ package View.Dono;
 import javax.swing.*;
 
 import Controller.Dono.CriarAcessoController;
-
+import java.util.InputMismatchException;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -124,7 +124,7 @@ public class CriarAcesso extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == ok){
             int confirmacao = JOptionPane.showConfirmDialog(rootPane,"Confirmar Criação?","Confirmação",2);
-            if(confirmacao == 0){
+            if(confirmacao == 0 && isCPF(tcpf.getText())){
                 String nome = tnome.getText();
                 int ap = Integer.parseInt(tap.getText());
                 int bloco = Integer.parseInt(tbloco.getText());
@@ -137,6 +137,8 @@ public class CriarAcesso extends JFrame implements ActionListener{
                 JOptionPane.showMessageDialog(rootPane, "Acesso Criado");
                 setVisible(false);
                 new DonoMenuView();
+            }else{
+                JOptionPane.showMessageDialog(rootPane, "CPF invalido");
             }
         }
         if(e.getSource() == cancelar){
@@ -144,4 +146,60 @@ public class CriarAcesso extends JFrame implements ActionListener{
             new DonoMenuView();
         }
     }
+
+    public static boolean isCPF(String CPF) {
+        // considera-se erro CPF's formados por uma sequencia de numeros iguais
+        if (CPF.equals("00000000000") ||
+            CPF.equals("11111111111") ||
+            CPF.equals("22222222222") || CPF.equals("33333333333") ||
+            CPF.equals("44444444444") || CPF.equals("55555555555") ||
+            CPF.equals("66666666666") || CPF.equals("77777777777") ||
+            CPF.equals("88888888888") || CPF.equals("99999999999") ||
+            (CPF.length() != 11))
+            return(false);
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+        // Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i=0; i<9; i++) {
+        // converte o i-esimo caractere do CPF em um numero:
+        // por exemplo, transforma o caractere '0' no inteiro 0
+        // (48 eh a posicao de '0' na tabela ASCII)
+            num = (int)(CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig10 = '0';
+            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+
+        // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for(i=0; i<10; i++) {
+            num = (int)(CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                 dig11 = '0';
+            else dig11 = (char)(r + 48);
+
+        // Verifica se os digitos calculados conferem com os digitos informados.
+            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+                 return(true);
+            else return(false);
+                } catch (InputMismatchException erro) {
+                return(false);
+            }
+        }
 }
